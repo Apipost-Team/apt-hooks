7eff77e99fa581c55dcf99e25ac292c7a9776c20
trqdz <trqdz@hotmail.com>
2023-03-20 10:52:10 +0800

    feat:新增npm自动化部署功能

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import useMemoizedFn from './useMemoizedFn'
+import useSafeState from './useSafeState'
+import useUnmountedRef from './useUnmountedRef'
+
+
+export default {
+  useMemoizedFn,
+  useSafeState,
+  useUnmountedRef
+}

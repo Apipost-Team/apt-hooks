144f6fbdbd4707c4e317e99d46650bbc60d81a17
trqdz <trqdz@hotmail.com>
2023-10-27 13:56:19 +0800

    feat: add firstMount hooks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import useMemoizedFn from './useMemoizedFn'
 import useSafeState from './useSafeState'
 import useUnmountedRef from './useUnmountedRef'
+import useFirstMountState from './useFirstMountState'
+
 
 
 export {
   useMemoizedFn,
   useSafeState,
-  useUnmountedRef
+  useUnmountedRef,
+  useFirstMountState
 }
